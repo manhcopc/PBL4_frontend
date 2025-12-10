45a583d593080dd9abf8521610922fd56fa5f89b
copc <102230084@sv1.dut.udn.vn>
2025-12-10 19:29:02 +0700

    feat: setup vercel proxy to bypass CORS

--- src/repository/auth/authRepository.js
+++ src/repository/auth/authRepository.js
@@ -1,28 +1,27 @@
-import axios from "axios";
 import api from "../../api/api";
 
-// const API_URL = "/api";
-const API_URL = import.meta.env.VITE_API_URL || "https://testmarkdb.azurewebsites.net/api";
-const publicClient = axios.create({
-  baseURL: API_URL,
-});
+// // const API_URL = "/api";
+// const API_URL = import.meta.env.VITE_API_URL || "https://testmarkdb.azurewebsites.net/api";
+// const publicClient = axios.create({
+//   baseURL: API_URL,
+// });
 
 export default function createAuthRepository() {
   return {
     async login(credentials) {
-      const res = await publicClient.post("/token/", credentials);
+      const res = await api.post("/token/", credentials);
       return res.data;
     },
     async requestPasswordReset(payload) {
-      const res = await publicClient.post("/SendOTPForVerify/", payload);
+      const res = await api.post("/SendOTPForVerify/", payload);
       return res.data;
     },
     async verifyOtp(payload) {
-      const res = await publicClient.post("/VerifyOTP/", payload);
+      const res = await api.post("/VerifyOTP/", payload);
       return res.data;
     },
     async resetPassword(payload) {
-      const res = await publicClient.post("/ResetPassword/", payload);
+      const res = await api.post("/ResetPassword/", payload);
       return res.data;
     },
     async changePassword(payload) {
