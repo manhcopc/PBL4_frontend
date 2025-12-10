641771fa5f0b45ad297c45016a584ecd5d9376cd
copc <102230084@sv1.dut.udn.vn>
2025-12-10 19:51:51 +0700

    fix: rename proxy path to /server to avoid vercel 405

--- src/data/clientApi.js
+++ src/data/clientApi.js
@@ -1,6 +1,6 @@
 import axios from "axios"; 
 // const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-const baseURL = "/api";
+const baseURL = "/server";
 // const baseURL = "https://testmarkdb.azurewebsites.net/api";
 console.log("Backend URL hien tai la: ", baseURL);
 
