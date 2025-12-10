b0a26b86775cc9b255a8c9daf885fe833d99ce73
manhcopc <102230084@sv1.dut.udn.vn>
2025-12-10 19:01:40 +0700

    Add console log for current backend URL
    Log the current backend URL to the console.

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios"; 
 // const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 const baseURL = "https://testmarkdb.azurewebsites.net/api";
+console.log("Backend URL hien tai la: ", baseURL);
 
 const api = axios.create({
   baseURL: baseURL,
