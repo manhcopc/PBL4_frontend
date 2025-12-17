cbb7733031c7f70da9379ceeb47907d1759ddc4b
copc <102230084@sv1.dut.udn.vn>
2025-12-17 10:42:23 +0700

    Fixed authRepository

--- src/repository/auth/authRepository.js
+++ src/repository/auth/authRepository.js
@@ -1,31 +1,37 @@
-import axios from "axios";
 import api from "../../api/api";
 
-const API_URL = "/api";
-const publicClient = axios.create({
-  baseURL: API_URL,
-});
+// const API_URL = "/api";
+// const publicClient = axios.create({
+//   baseURL: API_URL,
+// });
 
 export default function createAuthRepository() {
   return {
     async login(credentials) {
-      const res = await publicClient.post("/token/", credentials);
+      const res = await api.post("/token/", credentials);
       return res.data;
     },
+    //   const res = await publicClient.post("/token/", credentials);
+    //   return res.data;
+    // },
     async requestPasswordReset(payload) {
-      const res = await publicClient.post("/SendOTPForVerify/", payload);
+      const res = await api.post("/SendOTPForVerify/", payload);
       return res.data;
     },
     async verifyOtp(payload) {
-      const res = await publicClient.post("/VerifyOTP/", payload);
+      const res = await api.post("/VerifyOTP/", payload);
       return res.data;
     },
     async resetPassword(payload) {
-      const res = await publicClient.post("/ResetPassword/", payload);
+      const res = await api.post("/ResetPassword/", payload);
       return res.data;
     },
     async changePassword(payload) {
       const res = await api.post("/ChangePassword/", payload);
+      return res.data;
+    },
+    async logout() {
+      const res = await api.post("/logout/");
       return res.data;
     },
   };
