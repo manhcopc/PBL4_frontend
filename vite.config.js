ebad445f270fcb580411b0721bae2d213d3c9da7
copc <102230084@sv1.dut.udn.vn>
2025-12-17 11:02:54 +0700

    Add path link to google to test vercel

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "https://testmarkdb.azurewebsites.net", // 🌐 backend URL thật
-        changeOrigin: true, // giả origin để backend tưởng là cùng domain
-        secure: false, // bỏ check SSL (cần nếu dùng HTTPS tự ký)
-        rewrite: (path) => path.replace(/^\/api/, "/api"), // giữ nguyên đường dẫn
+      "/server": {
+        target: "https://testmarkdb.azurewebsites.net",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/server/, "/api"),
       },
     },
   },
